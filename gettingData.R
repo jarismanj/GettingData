eb035f76c474dc04ff0c54796d7ffb17802b2489
Abdullah Fathi <abdullahfathi@rocketmail.com>
2017-02-27 13:45:19 +0800

    Add Remark

--- gettingData.R
+++ gettingData.R
@@ -1,6 +1,9 @@
 library(readr)
 
 ## Download the data
-download.file("http://data.stats.gov.my/download_bptms.php?cat=1&id=34", destfile="./labourforce.csv", "auto")
+download.file("http://data.stats.gov.my/download_bptms.php?cat=1&id=34", destfile="./
+labourforce.csv", "auto")
+
+## Read CSV File
 labour_force <- read_csv("./labourforce.csv")
 View(labour_force)
