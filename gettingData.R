413ef131c47986e964b6ce51a3c1bfa48a414827
fakhirin <fakhirin.fauzi@hdcglobal.com>
2017-02-27 14:40:28 +0800

    Comment je
    No Comment

--- gettingData.R
+++ gettingData.R
@@ -1,4 +1,5 @@
 library(readr)
+#comment by Fakhirin.
 download.file("http://data.stats.gov.my/download_bptms.php?cat=1&id=34", destfile="./labourforce.csv", "auto")
 labour_force <- read_csv("./labourforce.csv")
 View(labour_force)
