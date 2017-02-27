1c001d8bf7922e9e714709c20f8b6aa833c4e157
niknizan <drnizan@gmail.com>
2017-02-27 13:34:10 +0800

    Added remarks to Jarisman's gettingData.R

--- gettingData.R
+++ gettingData.R
@@ -1,4 +1,6 @@
 library(readr)
+
+## Download the data
 download.file("http://data.stats.gov.my/download_bptms.php?cat=1&id=34", destfile="./labourforce.csv", "auto")
 labour_force <- read_csv("./labourforce.csv")
 View(labour_force)
